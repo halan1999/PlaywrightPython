bc97bb116b0edf779cc226dc96674d9a136bffaa
ThankNgan <ntthanhngan.2001@gmail.com>
2025-10-25 19:59:36 +0700

    remove files

--- Ducnt/my-playwright-project/Tests/test_bai_tap1.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import  Page, expect
-import re, time
-def test_bai_tap1(page: Page):
-    page.goto('https://voz.vn')
-    expect(page).to_have_title(re.compile('VOZ'))
-    print('Tieu de hop le')
-    expect(page).to_have_url(re.compile('voz.vn'))
-    print('URL hop le')
-    time.sleep(10)
