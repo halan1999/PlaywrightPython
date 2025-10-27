d91f0b2d00f7daa31e04f75b2be8618379e4b67d
ThankNgan <ntthanhngan.2001@gmail.com>
2025-10-28 02:34:09 +0700

    Update

--- ThanhNgan/BT_Buoi4/test_hr_department.py
+++ ThanhNgan/BT_Buoi4/test_hr_department.py
@@ -57,25 +57,3 @@ class TestDepartment:
         deparment_page.wait_for_selector("//form[@id='delete_record']")
         deparment_page.locator("//form[@id='delete_record']//button[normalize-space()='Confirm']").click()
         print("Delete department successfully")
-
-
-    # NOTE: Core HR
-    print("\nNavigate to the Core HR>Department")
-    # page.locator("//div[@class='hamburger-box']").click()
-    page.locator("//a[normalize-space()='Core HR']").click()
-    page.locator("//a[normalize-space()='Department']").click()
-
-    # Add New Department
-    departmentName = 'Thanh Ngân'
-    page.locator("//input[@placeholder='Name']").fill(departmentName)
-    page.locator("//span[normalize-space()='Save']").click()
-    # Search the the new added department
-    time.sleep(2)
-    page.locator("//input[@type='search']").fill(departmentName)
-    time.sleep(2)
-    results = page.locator("//table//tbody//tr").count()
-    if results > 0:
-        expect(page.locator("//table//tbody//tr[1]//td[1]")).to_have_text(departmentName)
-        print("Đã tìm thấy kết quả tìm kiếm")
-    else:
-        print("Không tìm thấy kết quả tìm kiếm")
