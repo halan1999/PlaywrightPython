e21af75a66eadfb453e2cb98ca7b1d45b081af10
fish4398 <hoaint4398@gmail.com>
2025-11-15 16:06:42 +0700

    update base_page

--- hoaint_1998/core/base_page.py
+++ hoaint_1998/core/base_page.py
@@ -11,67 +11,64 @@ class BasePage():
     #------ BASIC ACTION -------
     #--------------------------
 
-    def _get_locator(self, locator: str) -> Self:
-        self.page.locator(locator)
-        return self
+    def _get_locator(self, locator: str):
+        return self.page.locator(locator)
 
-    def _goto(self, url: str) -> Self:
-        self.page.goto(url, wait_until="domcontentloaded")
-        return self
+    def _goto(self, url: str) :
+        return self.page.goto(url, wait_until="domcontentloaded")
+        
     
-    def _click(self, locator: str) -> Self:
-        self.page.locator(locator).click()
-        return self
+    def _click(self, locator: str):
+        return self.page.locator(locator).click()
+        
     
-    def _dbclick(self, locator: str) -> Self:
-        self.page.locator(locator).dblclick()
-        return self
+    def _dbclick(self, locator: str):
+        return self.page.locator(locator).dblclick()
     
-    def _fill(self, locator: str, value: str) -> Self:
-        self.page.locator(locator).fill(value)
-        return self
+    def _fill(self, locator: str, value: str):
+        return self.page.locator(locator).fill(value)
     
     #--------------------------
     #------ LocatorAssertions = check the condition -------
     #--------------------------
-    def _expect_to_have_url(self, url: str) -> Self:
-        expect(self.page).to_have_url(re.compile(f".*{url}$"))
-        return self
+    def _expect_to_have_url(self, url: str):
+        return expect(self.page).to_have_url(re.compile(f".*{url}$"))
+      
     
-    def _expect_to_be_visible(self, locator: str) -> Self:
+    def _expect_to_be_visible(self, locator: str):
         """
         check whether an element is visible in the browser's viewport 
         """
-        expect(self.page.locator(locator)).to_be_visible()
-        return self
+        return expect(self.page.locator(locator)).to_be_visible()
+        
     
-    def _expect_to_be_hidden(self, locator: str) -> Self:
+    def _expect_to_be_hidden(self, locator: str):
         """
         check if element is hidden
         """
-        expect(self.page.locator(locator)).to_be_hidden()
-        return self
+        return expect(self.page.locator(locator)).to_be_hidden()
+        
     
-    def _expect_to_be_disabled(self, locator: str) -> Self:
+    def _expect_to_be_disabled(self, locator: str):
         """
         check whether an element is disabled (not interactable)
         """
-        expect(self.page.locator(locator)).to_be_disabled()
-        return self
+        return expect(self.page.locator(locator)).to_be_disabled()
+        
     
-    def _expect_to_be_enabled(self, locator: str) -> Self:
+    def _expect_to_be_enabled(self, locator: str):
         """
         check whether an element is enable (interactable)
         """
-        expect(self.page.locator(locator)).to_be_enabled()
-        return self 
+        return expect(self.page.locator(locator)).to_be_enabled()
+        
     
     #--------------------------
     #------ WAIT -------
     #--------------------------
-    def _wait_for_element(self, locator: str, timeout: int = 30000) -> Self:
-        self.page.locator(locator).wait_for(state="visible", timeout=timeout)
-        return self
+    def _wait_for_element(self, locator: str, timeout: int = 30000):
+        return self.page.locator(locator).wait_for(state="visible", timeout=timeout)
+        
     
     #--------------------------
     #------ TAKE A SCREENSHOT -------
