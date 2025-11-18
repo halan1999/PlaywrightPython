922d55bae85f01e85f92ce17673e54cc1695a759
Haodt3012 <133757095+Haodt3012@users.noreply.github.com>
2025-11-18 13:42:42 +0700

    Update BT_bai4.py

--- HaoDT/BT_Buoi4/BT_bai4.py
+++ HaoDT/BT_Buoi4/BT_bai4.py
@@ -8,16 +8,16 @@ def test_create_and_search_department():
         context = browser.new_context()
         page = context.new_page()
 
-        # 1. Mở trang
+        # Go to web
         page.goto("https://hrm.anhtester.com/erp/login")
 
-        # 2. Login (nếu có)
+        # Login
         page.fill("#iusername", "admin_example")
         page.fill("#ipassword", "123456")
         page.click("//button[@type='submit']")
         time.sleep(10)
     
-        # 3. Đi đến trang quản lý đơn vị
+        # Go to department page
         menu_locator = page.locator("//nav[@class='pc-sidebar light-sidebar']")
         menu_locator.evaluate("el => el.scrollTop = el.scrollHeight")  
         time.sleep(1)
@@ -28,7 +28,7 @@ def test_create_and_search_department():
         # Create department
         page.fill("//input[@placeholder='Name']", Department_Name)
 
-        # 6. Submit form
+        # Submit form
         page.click("//div[@class='card-footer text-right']//button[@type='submit']")
         time.sleep(10)
 
@@ -37,10 +37,8 @@ def test_create_and_search_department():
         time.sleep(10)
 
         # Check
-        # 9. Lấy dòng đầu tiên của bảng
         first_row = page.locator("//td[contains(@class,'sorting_1')]").inner_text()
 
-        # 10. Kiểm tra
         if "haodt1"in first_row:
             print("✔ Đơn vị đã được tạo và xuất hiện ở dòng 1!")
         else:
